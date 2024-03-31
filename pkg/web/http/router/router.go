bdc02c19d8ad6d37a7244f28a1dfd209c0c1b1c3
kohki miyama <kmx.111010@gmail.com>
2024-03-31 15:33:46 +0900

    feat: :sparkles: make router file

---
+++ pkg/web/http/router/router.go
@@ -0,0 +1,40 @@
+package router
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/km1110/task-copilot-server/pkg/infrastructure/database"
+	"github.com/km1110/task-copilot-server/pkg/web/http/controller"
+)
+
+func InitRouter() {
+	db, err := database.NewDB()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	srv := &http.Server{Addr: "0.0.0.0:8080", Handler: newHandlers(db)}
+
+	// TODO: graceful shutdown
+
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("HTTP server ListenAndServe: %v", err)
+	}
+}
+
+func newHandlers(db *sql.DB) http.Handler {
+	r := chi.NewRouter()
+
+	r.Route("/todos", func(r chi.Router) {
+		r.Get("", controller.GetTodos(db))
+		// r.Post("", controller.CreateTodo(db))
+		// r.Patch("/{todoID}", controller.UpdateTodo(db))
+		// r.Delete("/{todoID}", controller.DeleteTodo(db))
+	})
+
+	return r
+}
