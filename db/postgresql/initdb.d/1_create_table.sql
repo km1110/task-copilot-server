6f4c69cb118887302a1a4b3d3f6f12897d1e1f4e
kohki miyama <kmx.111010@gmail.com>
2024-04-08 21:13:21 +0900

    feat: :zap: update table and insert data

--- db/postgresql/initdb.d/1_create_table.sql
+++ db/postgresql/initdb.d/1_create_table.sql
@@ -1,18 +1,81 @@
 CREATE TABLE IF NOT EXISTS "users" (
-    "id"      UUID NOT NULL,
-    "name"    VARCHAR(255) NOT NULL,
-    "role_id" UUID NOT NULL,
-    "status"  BOOLEAN DEFAULT 'true' NOT NULL,
+    "id"         UUID NOT NULL,
+    "role_id"    UUID NOT NULL,
+    "name"       VARCHAR(255) NOT NULL,
+    "is_active"  BOOLEAN DEFAULT 'true' NOT NULL,
     PRIMARY KEY("id")
+    FOREIGN KEY("role_id") REFERENCES "roles"("id")
 );
 
 CREATE TABLE IF NOT EXISTS "todos" (
-    "id"          UUID NOT NULL,
-    "user_id"     UUID NOT NULL,
-    "name"        VARCHAR(255) NOT NULL,
-    "target_date" TIMESTAMP NOT NULL,
-    "done_date"   TIMESTAMP,
-    "status"      BOOLEAN DEFAULT 'false' NOT NULL,
+    "id"                UUID NOT NULL,
+    "user_id"           UUID NOT NULL,
+    "name"              VARCHAR(255) NOT NULL,
+    "target_date"       TIMESTAMP NOT NULL,
+    "done_date"         TIMESTAMP NOT NULL,
+    "is_completed"      BOOLEAN DEFAULT 'false' NOT NULL,
     PRIMARY KEY ("id"),
     FOREIGN KEY("user_id") REFERENCES "users"("id") 
+);
+
+CREATE TABLE IF NOT EXISTS "calendar" (
+    "id"            UUID NOT NULL,
+    "calendar_name" VARCHAR(255) NOT NULL,
+    "created_at"    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at"    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY("id")
+)
+
+CREATE TABLE IF NOT EXISTS "events" (
+    "id"            UUID NOT NULL,
+    "user_id"       UUID NOT NULL,
+    "tag_id"        UUID NOT NULL,
+    "title"         VARCHAR(255) NOT NULL,
+    "start_date"    TIMESTAMP NOT NULL,
+    "end_date"      TIMESTAMP NOT NULL,
+    "created_at"    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at"    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY("id"),
+    FOREIGN KEY("user_id") REFERENCES "users"("id")
+);
+
+CREATE TABLE IF NOT EXISTS "roles" (
+    "id"            UUID NOT NULL,
+    "role_name"     VARCHAR(255) NOT NULL,
+    "created_at"    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at"    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY("id")
+);
+
+CREATE TABLE IF NOT EXISTS "tags" (
+    "id"            UUID NOT NULL,
+    "user_id"       UUID NOT NULL,
+    "tag_name"      VARCHAR(255) NOT NULL,
+    "tag_color"     VARCHAR(255) NOT NULL,
+    "created_at"    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at"    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY("id")
+);
+
+CREATE TABLE IF NOT EXISTS "user_calendar" (
+    "id"            UUID NOT NULL,
+    "user_id"       UUID NOT NULL,
+    "calendar_id"   UUID NOT NULL,
+    "access_level"  enum('read', 'write', 'admin') NOT NULL,
+    "created_at"    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at"    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY("id"),
+    FOREIGN KEY("user_id") REFERENCES "users"("id"),
+    FOREIGN KEY("calendar_id") REFERENCES "calendar"("id")
+);
+
+CREATE TABLE IF NOT EXISTS "calendar_event" (
+    "id"            UUID NOT NULL,
+    "calendar_id"   UUID NOT NULL,
+    "event_id"      UUID NOT NULL,
+    "created_at"    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at"    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY("id"),
+    FOREIGN KEY("calendar_id") REFERENCES "calendar"("id"),
+    FOREIGN KEY("event_id") REFERENCES "events"("id")
 );
--- db/postgresql/initdb.d/2_insert_data.sql
+++ db/postgresql/initdb.d/2_insert_data.sql
@@ -6,3 +6,31 @@ VALUES ('b9d4a4ab-ea45-d22f-3ed6-46c32ec8b2b1','kohki', 'b9d4a4ab-ea45-d22f-3ed6
 INSERT INTO todos ("id","user_id","name","target_date","done_date","status")
 VALUES ('e1d388da-47e0-ca1d-2722-0a41d93f872f','b9d4a4ab-ea45-d22f-3ed6-46c32ec8b2b1','task1','2021-01-01 00:00:00','2021-01-01 00:00:00','true'),
         ('91d1a74f-8d0e-7287-b218-31e8fdaf8ad5','b9d4a4ab-ea45-d22f-3ed6-46c32ec8b2b1','task2', '2021-01-01 00:00:00', '2021-01-01 00:00:00','true');
+
+-- calendar
+INSERT INTO calendar ("id","calendar_name")
+VALUES ('e1d388da-47e0-ca1d-2722-0a41d93f852f','parsonal');
+
+-- events
+INSERT INTO events ("id","user_id","tag_id","title","start_date","end_date")
+VALUES ('e1d388da-47e6-ca1d-2722-0a41d93f872f','b9d4a4ab-ea45-d22f-3ed6-46c32ec8b2b1','e1d388da-47e0-ca1d-2722-0a41d93f872h','event1','2024-05-01 12:00:00','2024-05-01 15:00:00'),
+        ('e1d388da-47e0-ca1d-2722-0a41d93f872g','b9d4a4ab-ea45-d22f-3ed6-46c32ec8b2b1','g1d388da-47e0-ca1d-2722-0a41d93f872f','event2','2024-05-02 00:00:00','2024-05-06 00:00:00');
+
+-- roles
+INSERT INTO roles ("id","role_name")
+VALUES ('b9d4a4ab-ea45-d22f-3ed6-46c32ec8b2f5','admin');
+
+-- tags
+INSERT INTO tags ("id","user_id","tag_name","tag_color")
+VALUES ('e1d388da-47e0-ca1d-2722-0a41d93f872h','b9d4a4ab-ea45-d22f-3ed6-46c32ec8b2b1','school','#ff0000'),
+        ('e1d388da-47e0-ca1d-2722-0a41d93f872g','b9d4a4ab-ea45-d22f-3ed6-46c32ec8b2b1','part-time','#00ff00');
+
+-- user_calendar
+INSERT INTO user_calendar ("id","user_id","calendar_id","access_level")
+VALUES ('e1d388da-47e0-ca1d-2722-0a41d93g872f','b9d4a4ab-ea45-d22f-3ed6-46c32ec8b2b1','e1d388da-47e0-ca1d-2722-0a41d93f852f','admin');
+
+-- calendar_events
+INSERT INTO calendar_events ("id", "calendar_id","event_id")
+VALUES ('e1d388da-47e0-ca1d-2722-0a41d73f872f','e1d388da-47e0-ca1d-2722-0a41d93f852f','e1d388da-47e6-ca1d-2722-0a41d93f872f'),
+        ('e1d388da-47e0-ca1d-2722-0a41d93f872g','e1d388da-47e0-ca1d-2722-0a41d93f852f','e1d388da-47e0-ca1d-2722-0a41d93f872g');
+```
