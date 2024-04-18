4e4ccf2edf99b2147a36d6749681b953dfc71494
kohki miyama <kmx.111010@gmail.com>
2024-04-18 17:40:01 +0900

    feat: :zap: validator DI

--- api/pkg/infrastructure/router/router.go
+++ api/pkg/infrastructure/router/router.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/km1110/task-copilot-server/pkg/adapter/controller"
 	"github.com/km1110/task-copilot-server/pkg/adapter/repository"
+	"github.com/km1110/task-copilot-server/pkg/infrastructure/validation"
 	"github.com/km1110/task-copilot-server/pkg/usecase"
 )
 
@@ -17,7 +18,8 @@ func InitRouter(db *sql.DB) *gin.Engine {
 
 	// todo DPI
 	todoRepository := repository.NewTodoRepository(db)
-	todoUsecase := usecase.NewTodoUsecase(todoRepository)
+	todoValidator := validation.NewTodoValidator()
+	todoUsecase := usecase.NewTodoUsecase(todoRepository, todoValidator)
 	todoController := controller.NewTodoController(todoUsecase)
 
 	initTodoRouter(&g.RouterGroup, todoController)
