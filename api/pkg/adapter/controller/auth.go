3fae2dbf02c0e8b94b362e524e3d92872e6a25ff
kohki miyama <kmx.111010@gmail.com>
2024-05-25 13:30:29 +0900

    feat: :zap: functionalize common processes

--- api/pkg/adapter/controller/auth.go
+++ api/pkg/adapter/controller/auth.go
@@ -1,16 +1,14 @@
 package controller
 
 import (
-	"context"
-	"log"
+	"errors"
 	"net/http"
 	"strings"
 
-	firebase "firebase.google.com/go"
 	"github.com/gin-gonic/gin"
 	"github.com/km1110/task-copilot-server/pkg/domain/model"
+	"github.com/km1110/task-copilot-server/pkg/infrastructure/firebase"
 	"github.com/km1110/task-copilot-server/pkg/usecase"
-	"google.golang.org/api/option"
 )
 
 type IAuthController interface {
@@ -19,45 +17,22 @@ type IAuthController interface {
 }
 
 type authController struct {
-	au usecase.IAuthUsecase
+	au          usecase.IAuthUsecase
+	firebaseApp firebase.IFirebaseApp
 }
 
-func NewAuthController(au usecase.IAuthUsecase) IAuthController {
-	return &authController{au: au}
+func NewAuthController(au usecase.IAuthUsecase, firebaseApp firebase.IFirebaseApp) IAuthController {
+	return &authController{au: au, firebaseApp: firebaseApp}
 }
 
 func (ac *authController) Login(c *gin.Context) {
-	opt := option.WithCredentialsFile("./service_accout_key.json")
-	app, err := firebase.NewApp(context.Background(), nil, opt)
-	if err != nil {
-		log.Fatalf("error initializing app: %v\n", err)
-	}
+	token, err := validateAuthHeader(c)
 
-	firebaseAuth, err := app.Auth(context.Background())
-	if err != nil {
-		log.Fatalf("error getting Auth client: %v\n", err)
-	}
-
-	authHeader := c.GetHeader("Authorization")
-	if authHeader == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Authorization header required"})
-		return
-	}
-
-	token := strings.TrimPrefix(authHeader, "Bearer ")
-	if token == authHeader {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid token format"})
-		return
-	}
-
-	verifiedToken, err := firebaseAuth.VerifyIDToken(c, token)
+	uid, err := ac.firebaseApp.VerifyIDToken(c, token)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid token"})
 		return
 	}
-
-	firebaseUID := verifiedToken.UID
-	uid := firebaseUID
 
 	msg, err := ac.au.Login(c, uid)
 	if err != nil {
@@ -69,12 +44,21 @@ func (ac *authController) Login(c *gin.Context) {
 }
 
 func (ac *authController) Register(c *gin.Context) {
-	firebaseUID, _ := c.Get("firebaseUID")
-	uid := firebaseUID.(string)
-
 	user := model.User{}
 	if err := c.Bind(&user); err != nil {
 		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	token, err := validateAuthHeader(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	uid, err := ac.firebaseApp.VerifyIDToken(c, token)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid token"})
 		return
 	}
 
@@ -87,4 +71,18 @@ func (ac *authController) Register(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, msg)
+}
+
+func validateAuthHeader(c *gin.Context) (string, error) {
+	authHeader := c.GetHeader("Authorization")
+	if authHeader == "" {
+		return "", errors.New("Authorization header required")
+	}
+
+	token := strings.TrimPrefix(authHeader, "Bearer ")
+	if token == authHeader {
+		return "", errors.New("Invalid token format")
+	}
+
+	return token, nil
 }
