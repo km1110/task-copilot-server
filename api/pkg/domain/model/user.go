dd69a6f21db06042228d9d5b24a5fe74a3a3dfc3
kohki miyama <kmx.111010@gmail.com>
2024-06-06 19:50:05 +0900

    feat: :zap: add role model and fix user role columns

--- api/pkg/domain/model/user.go
+++ api/pkg/domain/model/user.go
@@ -1,41 +1,9 @@
 package model
 
-import "github.com/google/uuid"
-
 type User struct {
 	ID          string
 	FirebaseUID string
 	Name        string
-	RoleID      string
+	Role        Role
 	Status      bool
-}
-
-func NewUser(id, name, role_id string, status bool) *User {
-	return &User{
-		ID:     id,
-		Name:   name,
-		RoleID: role_id,
-		Status: status,
-	}
-}
-
-func NewUserID() string {
-	return uuid.NewString()
-}
-
-func IsValidUserID(id string) bool {
-	_, err := uuid.Parse(id)
-	return err == nil
-}
-
-type UserWithTodos struct {
-	ID    string
-	Todos []*Todo
-}
-
-func NewUserWithTodos(u *User, todos []*Todo) *UserWithTodos {
-	return &UserWithTodos{
-		ID:    u.ID,
-		Todos: todos,
-	}
 }
